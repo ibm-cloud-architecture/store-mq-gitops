4c2a2dbce30096386f5213f10236c4b5d07c3866
Jerome Boyer <jerome.boyer@gmail.com>
2021-10-26 15:36:01 -0700

    modify job and service

--- bootstrap/scripts/copySecrets.sh
+++ bootstrap/scripts/copySecrets.sh
@@ -1,3 +1,3 @@
 YOUR_PROJECT_NAME=$1
 oc project $YOUR_PROJECT_NAME
-oc get secret ibm-entitlement-key -n openshift-operators -o json | jq -r '.metadata.namespace="'${YOUR_PROJECT_NAME}'"' | oc apply -f -
+oc get secret ibm-entitlement-key -n ibm-common-services -o json | jq -r '.metadata.namespace="'${YOUR_PROJECT_NAME}'"' | oc apply -f -
